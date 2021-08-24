498615d517f038a70a77bb35783b03b0efedf7e2
Svetoslav Gochev <60218470+SvetoslavGochev@users.noreply.github.com>
2021-08-24 15:53:11 +0300

    gg
    Added new test for controller of ConfidentialityTest and ConnectionTest.

---
+++ МоитеГуми.Test/Controllers/ConfidentialityTest.cs
@@ -0,0 +1,17 @@
+﻿namespace МоитеГуми.Test.Controllers
+{
+    using MyTested.AspNetCore.Mvc;
+    using Xunit;
+    using МоитеГуми.Controllers;
+
+    public class ConfidentialityTest
+    {
+        [Fact]
+        public void CreateShouldReturnViewOnPost()
+           => MyController<ConfidentialityController>
+           .Instance()
+           .Calling(c => c.Index())
+           .ShouldReturn()
+           .View();
+    }
+}
---
+++ МоитеГуми.Test/Controllers/ConnectionTest.cs
@@ -0,0 +1,17 @@
+﻿using MyTested.AspNetCore.Mvc;
+using Xunit;
+using МоитеГуми.Controllers;
+
+namespace МоитеГуми.Test.Controllers
+{
+    public class Connectiontest
+    {
+        [Fact]
+        public void CreateShouldReturnViewOnPost()
+            => MyController<ConnectionController>
+            .Instance()
+            .Calling(c => c.Index())
+            .ShouldReturn()
+            .View();
+    }
+}
