353b1f0a0ae46dd69e94fe26677ad5843ff24804
Svetoslav Gochev <60218470+SvetoslavGochev@users.noreply.github.com>
2021-09-15 13:39:47 +0300

    Za area
    gg

---
+++ МоитеГуми/Migrations/20210910114400_proba.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace МоитеГуми.Migrations
+{
+    public partial class proba : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
